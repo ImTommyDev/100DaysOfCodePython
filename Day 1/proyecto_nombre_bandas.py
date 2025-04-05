c453fb6399d65fb97f97b509d3989aff2e5388b0
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-05 17:55:10 +0200

    Primer commit con el primer dia completado

---
+++ Day 1/proyecto_nombre_bandas.py
@@ -0,0 +1,5 @@
+nombre_ciudad = input("¿Cuál es el nombre de la ciudad? ")
+nombre_mascota = input("¿Cuál es el nombre de la mascota? ")
+
+nombre_banda = nombre_ciudad + " " + nombre_mascota
+print(f'El nombre de la banda es {nombre_banda}')
---
+++ Day 1/strings.py
@@ -0,0 +1,4 @@
+usuario = input('Introduce tu nombre de usuario: ')
+length = len(usuario)
+# Convertir a string la longitud del nombre de usuario
+print(f'Hola {usuario}, tu nombre tiene {length} caracteres.')
