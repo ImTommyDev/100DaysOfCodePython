b4efbb54e13d5bcbe8fc9f663aba030bac2b5a3f
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-09 16:30:35 +0200

    Dia 6 completado: Se han visto funciones y hemos practicado con bucles usando Reeborgs world

---
+++ Day 6/funciones.py
@@ -0,0 +1,16 @@
+def funcion():
+    print("Hola desde la función")
+    
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+print(funcion())
+print(suma(5, 3))
+print(resta(10, 4))
+print(multiplicacion(2, 3))
