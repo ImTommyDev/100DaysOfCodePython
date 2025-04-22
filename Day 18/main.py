acc0adf4b97799bc895be5163dc00f7f3bd23ee7
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-22 16:47:36 +0200

    Drawing a square with dashed lines

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,22 +1,24 @@
 from turtle import Turtle, Screen
 
-
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("coral")
+timmy_the_turtle.color("black")
 
-#Dibujo un cuadrado
+#Drawing a square with dashed lines
 timmy_the_turtle.speed(1)
 for i in range(4):
-    timmy_the_turtle.forward(100)
+    for i in range (5):
+        timmy_the_turtle.forward(10)
+        timmy_the_turtle.penup()
+        timmy_the_turtle.forward(10)
+        timmy_the_turtle.pendown()
+        
+    
     timmy_the_turtle.right(90)
 
 
 
-
-
-
-
+#Drawing different shapes
 
 
 
