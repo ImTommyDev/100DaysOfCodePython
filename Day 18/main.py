8c1cae288c915392c44b251c3f16594ddde56068
ImTommyDev <tommy.devlpr@gmail.com>
2025-04-22 16:34:34 +0200

    Practicando con turtle, GUI

---
+++ Day 18/main.py
@@ -0,0 +1,40 @@
+from turtle import Turtle, Screen
+
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("coral")
+
+#Dibujo un cuadrado
+timmy_the_turtle.speed(1)
+for i in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
